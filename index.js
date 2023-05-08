804d92edee0c589eb9148b4fc4845860a879a8bf
Marlon Hamaekers <mar.hamaekers@gmail.com>
2023-05-08 14:02:11 +0200

    edit

--- index.js
+++ index.js
@@ -13,17 +13,29 @@ const income = [];
 // kosten invoer lezen van moment costBtn geklikt wordt
 costBtn.addEventListener("click", addingCosts);
 incomeBtn.addEventListener("click", addingIncome);
+costBtn.addEventListener("click", addingCosts);
+incomeBtn.addEventListener("click", addingIncome);
+
+
+// KeyboardEvent.code Read only
+// Returns a string with the code value of the physical key represented by the event.
+function handleInputEvent(event) {
+  if (event.code === "Enter" )
+}
+
 
 
 // Mapping of costs 
 function addingCosts() {
   costs.push(Number(costInput.value));
-  console.log(costs);
     const totalCost = costs.map((mov) => mov).reduce((acc, curr) => acc + curr);
+    costInput.value = "";
+    costInput.focus();
     console.log(totalCost);
 }
 
 function addingIncome() {
   income.push(Number(incomeInput.value));
-  console.log(income);
+  const totalIncome = income.map((mov) => mov).reduce((acc, curr) => acc + curr);
+  console.log(totalIncome);
 }
