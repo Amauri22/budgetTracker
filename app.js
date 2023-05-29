c34416a79d1e98ead84ba32a8a69bd91971476cb
Marlon Hamaekers <marlonhamaekers@air-van-marlon.home>
2023-05-29 20:12:37 +0200

    add list, income, and expense

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ const income = document.getElementById("income");
 const incomeBtn = document.getElementById("income-btn");
 const expenseBtn = document.getElementById("expense-btn");
 const expense = document.getElementById("expense");
-
-
-
-
+const balance = document.getElementById("balance")
 
 class User {
     constructor(name) {
@@ -52,11 +49,27 @@ let marlon = new User("Marlon Hamaekers");
 
 const inputContainer = document.querySelector(".inputs");
 
-inputContainer.addEventListener("click", function(e) {
+inputContainer.addEventListener("click", function (e) {
     e.preventDefault();
+
+    // Get the id of the element
     const clicked = e.target.getAttribute("id");
+    // check if id is one of the buttons due to event
     if (clicked === "expense-btn" || clicked === "income-btn") {
-        clicked = "expense-btn"? console.log("hey")
+        // get the element
+        const el = document.getElementById(clicked).dataset.tab;
         
+        // storing in the correct category
+        const movement = document.querySelector(`.${el}-movements`);
+        const movementValue = Number(movement.value);
+        movement === income ? marlon.addIncome(movementValue) : marlon.addExpense(movementValue);
+        balance.textContent = marlon.calculateBalance()
+        marlon.movements.push(movementValue);
+        
+        // add to movementslist and add a - when expense
+        const movementsList = document.querySelector(".movements-list")
+        const html = `${movement === income ? `<li>${movementValue}</li>`: `<li> -${movementValue}</li>`}`;
+        movementsList.insertAdjacentHTML("afterbegin", html);
+
     }
 })
